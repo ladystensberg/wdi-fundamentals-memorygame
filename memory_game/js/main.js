7d40bf18b2edd1b27969ef97b0498a4412dcde86
Sarah Stensberg <ladystensberg@gmail.com>
2018-09-03 16:54:46 -0700

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,21 +1,22 @@
 var cards = ["queen", "queen", "king", "king"];
 var cardsInPlay = [];
 
-var cardOne = cards[0];
-var cardTwo = cards[1];
-
-cardsInPlay.push(cardOne);
-cardsInPlay.push(cardTwo);
-
-if (cardsInPlay.length === 2) {
-  if (cardsInPlay[0] === cardsInPlay[1]) {
-    alert("You found a match!");
-  } else {
-    alert("Sorry, try again.");
+function checkForMatch() {
+  if (cardsInPlay.length === 2) {
+    if (cardsInPlay[0] === cardsInPlay[1]) {
+      alert("You found a match!");
+    } else {
+      alert("Sorry, try again.");
+    }
   }
 }
 
 
-console.log("User flipped " + cardOne);
-console.log("User flipped " + cardTwo);
-console.log(cardsInPlay);
+function flipCard (cardId) {
+  console.log("User flipped " + cards[cardId]);
+  cardsInPlay.push(cardId);
+  checkForMatch();
+}
+
+flipCard(0);
+flipCard(2);
